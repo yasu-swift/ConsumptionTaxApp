57cdf4b7a600bf50731b3563d3304140e842932f
yasu-swift <sp4c72q9@icloud.com>
2021-11-09 20:36:56 +0900

    Initial Commit

---
+++ ConsumptionTaxApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConsumptionTaxApp
+//
+//  Created by 高橋康之 on 2021/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
